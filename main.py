9648c1e95ca892a65d31fae666643cb4ce572ecc
isselmou-nemine <isselmou.nem@gmail.com>
2022-02-14 14:25:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
+
 from dotenv import load_dotenv
 import os
 load_dotenv()
+SAMPLE_SPREADSHEET_ID = os.getenv("SAMPLE_SPREADSHEET_ID")
+API_KEY_TEQUILA = os.getenv("API_KEY_TEQUILA")
+GOOGLE_SHEET_API_KEY= os.getenv("GOOGLE_SHEET_API_KEY")
+
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-
 SERVICE_ACCOUNT_FILE = 'keys.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 creds = None
 creds = service_account.Credentials.from_service_account_file(
     SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-# If modifying these scopes, delete the file token.json.
-
-
-# The ID spreadsheet.
-SAMPLE_SPREADSHEET_ID = os.getenv("SAMPLE_SPREADSHEET_ID")
-API_KEY_TEQUILA = os.getenv("API_KEY_TEQUILA")
-GOOGLE_SHEET_API_KEY= os.getenv("GOOGLE_SHEET_API_KEY")
-
-
 service = build('sheets', 'v4', credentials=creds)
 
 # Call the Sheets API
@@ -26,71 +20,65 @@ sheet = service.spreadsheets()
 result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="prices!A1:C11").execute()
 values = result.get('values', [])
-#print(values)
-
-# iata=[]
-# for i in range(0,9):
-#     iata.append(["TESTING"])
-# print(f"iata: {iata}")
-#
-# result = sheet.values().update(
-#     spreadsheetId=SAMPLE_SPREADSHEET_ID, range='prices!B2:B10',
-#     valueInputOption="USER_ENTERED", body={"values":iata}).execute()
-
 
 sheet_data = values
-
 prices = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                             range="prices!C2:C11").execute()
-#print("prices")
-
 import requests
 cities = []
 iatas = []
 for i in range(1, len(values)):
     cities.append(values[i][0])
-#print(cities)
+
+
+#geeting IATA of each city in my google sheet (International Air Transport Association code)
 
 for city in cities:
     headers = {
-        "apikey": API_KEY_TEQUILA ,
+        "apikey": API_KEY_TEQUILA,
     }
 
     query = {
         "term": city,
-
     }
-    #print(city)
+
     response = requests.get(url="https://tequila-api.kiwi.com/locations/query", headers=headers, params=query)
     results = response.json()
     #print(results)
     iatas.append(results["locations"][0]['code'])
-
 #print(iatas)
+
 iatas_list = []
 for iata in iatas:
     iatas_list.append([iata])
 #print(iatas_list)
 
+#writing in my google sheet IATAS that we get back from taquila API
+
 result = sheet.values().update(
     spreadsheetId=SAMPLE_SPREADSHEET_ID, range='prices!B2:B11',
     valueInputOption="USER_ENTERED", body={"values": iatas_list}).execute()
 endpoint = "https://tequila-api.kiwi.com/v2/search"
-import requests
 
-headers = {
-    "apikey": GOOGLE_SHEET_API_KEY,
-}
 
-superior_price = None
+#getting the date of today and next 6 months from today
+
 from datetime import datetime, timedelta
 current_date=datetime.now()
 next_six_months = current_date + timedelta(days=90)
 next_six_months = next_six_months.strftime("%d/%m/%Y")
 current_date = current_date.strftime("%d/%m/%Y")
 
+#making request to taquila api to get data in case the price is lower then the price that i have in google sheet.
+#note that i'm intrested in just in all flights from berlin to the destination cities in google sheet.
+#period between my deperature and return should be from 7 to 28 nights.
+
+import requests
+headers = {
+    "apikey": GOOGLE_SHEET_API_KEY,
+}
+superior_price = None
 for iata in iatas:
-    #print(iata)
     if iata == "BER":
         pass
     else:
@@ -116,18 +104,18 @@ for iata in iatas:
                                         range="prices!C2:C9").execute()
             prices = prices["values"]
 
-            print(prices)
+            #print(prices)
             prices_list = []
             for list in prices:
                 prices_list.append(int(list[0]))
-            print(prices_list)
+            #print(prices_list)
             for price in prices_list:
                 if price > response.json()["data"][0]["price"]:
                     inferior_price = response.json()["data"][0]["price"]
                     superior_price = True
             if superior_price==True:
+                print("___________________________####___________________________")
                 print(formated_message)
-                print("______________________________________________________")
                 stops_data = response.json()["data"][0]["route"]
                 stop_city = f"{stops_data[0]['cityTo']}-{stops_data[0]['flyTo']}"
                 stop_city_name=stops_data[0]['cityTo']
@@ -136,79 +124,27 @@ for iata in iatas:
                 airport_to = response.json()["data"][0]["flyTo"]
                 city_from = response.json()["data"][0]["cityFrom"]
                 city_to = response.json()["data"][0]["cityTo"]
-                formated_message = f'{city_to}-{airport_to}' \
-                                   f'with one stop at {stop_city}'
+                formated_message =  f' with only {price}euro! ' \
+                                    f'from {city_from}-{airport_from} to ' \
+                                    f'{city_to}-{airport_to}' \
+                                    f'with one stop at {stop_city}.\n' \
+                                    f'Wish you a nice trip!'
+
                 if city_to != stop_city_name:
                     print(formated_message)
-                print("______________________________________________________")
+                print("_____________________@@@_________________________________")
+                import smtplib
+                my_email = "first.user799@gmail.com"
+                password = "Azerty1234."
+                #Now i will send an email alert to the costumer with the relevant data
+                with smtplib.SMTP("smtp.gmail.com") as connection:
+                    connection.starttls()
+                    connection.login(user=my_email, password=password)
+                    connection.sendmail(from_addr=my_email, to_addrs="first.user79@yahoo.com",
+                                        msg=f"subject:Dear costumer\n\n {formated_message}")
+                print("email sent")
             else:
                 pass
         except IndexError:
             print(f"there is no flight with the required parameters to {iata}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
